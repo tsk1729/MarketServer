ffc92bad4e9a5c3df14dc8c745d63f62e87d034b
Sandeep <talapally.kumar@oracle.com>
2025-08-25 00:26:30 +0530

    cors init

--- creators/controller.py
+++ creators/controller.py
@@ -4,13 +4,12 @@ from fastapi import APIRouter, Body
 
 from creators import service
 from creators.models import FormData
-from mongo import repo_manager
 
 creator_controller = APIRouter(tags=["Webhooks API"])
 @creator_controller.post("/creator")
-async def save_creator_data(data:FormData,user_id: UUID = Body()):
-    return await service.save_creator(data,str(user_id))
+def save_creator_data(data:FormData,user_id: UUID = Body()):
+    return service.save_creator(data,str(user_id))
 
 @creator_controller.get("/creator")
-async def get_creator_data(user_id:str):
-    return await service.get_creator(str(user_id))
+def get_creator_data(user_id:str):
+    return service.get_creator(str(user_id))
--- mongo.py
+++ mongo.py
@@ -1,66 +1,66 @@
 import urllib
 from datetime import datetime
-
-from pymongo import  MongoClient
+from pymongo import MongoClient
 from typing import Any
 import certifi
-from pymongo.synchronous.collection import Collection
+from pymongo.collection import Collection  # <- corrected import
 
 username = "sandeep"
 password = "peedn@sT7"
 encoded_username = urllib.parse.quote_plus(username)
 encoded_password = urllib.parse.quote_plus(password)
-MONGO_STRING = f"mongodb+srv://{encoded_username}:{encoded_password}@owlit-cluster.s4cvt.mongodb.net/?retryWrites=true&w=majority&appName=owlit-cluster"
+
+MONGO_STRING = (
+    f"mongodb+srv://{encoded_username}:{encoded_password}"
+    "@owlit-cluster.s4cvt.mongodb.net/?retryWrites=true&w=majority&appName=owlit-cluster"
+)
 DATABASE_NAME = "owlit"
+
 
 class MongoRepository:
     def __init__(self, collection: Collection):
         self.collection = collection
 
-    async def create(self, data: dict) -> Any:
-        return await self.collection.insert_one(data)
+    def create(self, data: dict) -> Any:
+        return self.collection.insert_one(data)
 
-    async def read(self, query: dict) -> Any:
-        return await self.collection.find_one(query)
+    def read(self, query: dict) -> Any:
+        return self.collection.find_one(query)
 
-    async def read_all(self, query=None) -> iter:
+    def read_all(self, query=None) -> iter:
         if query is None:
             query = {}
         return self.collection.find(query)
 
-    async def update(self, query: dict, update_data: dict) -> Any:
-        return await self.collection.update_one(query, {"$set": update_data})
+    def update(self, query: dict, update_data: dict) -> Any:
+        return self.collection.update_one(query, {"$set": update_data})
 
-    async def delete(self, query: dict) -> Any:
-        return await self.collection.delete_one(query)
+    def delete(self, query: dict) -> Any:
+        return self.collection.delete_one(query)
 
-    async def delete_all(self, query: dict) -> Any:
-        return await self.collection.delete_many(query)
+    def delete_all(self, query: dict) -> Any:
+        return self.collection.delete_many(query)
 
-    async def upsert(self, query: dict, update_data: dict) -> Any:
-        return await self.collection.update_one(query, {"$set": update_data}, upsert=True)
+    def upsert(self, query: dict, update_data: dict) -> Any:
+        return self.collection.update_one(query, {"$set": update_data}, upsert=True)
 
-    async def update_with_ops(self, filter_query: dict, update_operations: dict) -> Any:
-        return await self.collection.update_many(filter_query, update_operations)
+    def update_with_ops(self, filter_query: dict, update_operations: dict) -> Any:
+        return self.collection.update_many(filter_query, update_operations)
 
-    async def add_to_array_field(self, query: dict, array_field: str, item_data: dict) -> Any:
-        update_data = {
-            "$addToSet": {array_field: item_data}
-        }
-        return await self.collection.update_one(query, update_data, upsert=True)
+    def add_to_array_field(self, query: dict, array_field: str, item_data: dict) -> Any:
+        update_data = {"$addToSet": {array_field: item_data}}
+        return self.collection.update_one(query, update_data, upsert=True)
 
-    async def remove_from_array_field(self, query: dict, array_field: str, item_data: dict) -> Any:
-        update_data = {
-            "$pull": {array_field: item_data}
-        }
-        return await self.collection.update_one(query, update_data)
+    def remove_from_array_field(self, query: dict, array_field: str, item_data: dict) -> Any:
+        update_data = {"$pull": {array_field: item_data}}
+        return self.collection.update_one(query, update_data)
 
 
 class JobRepository:
     def __init__(self, collection: Collection):
         self.collection = collection
 
-    async def add_job(self, id: str, user_id: str, func_name: str, args: dict, target_time: datetime):
+    def add_job(self, id: str, user_id: str, func_name: str, args: dict, target_time: datetime):
         job_data = {
             "_id": id,
             "user_id": user_id,
@@ -68,31 +68,29 @@ class JobRepository:
             "args": args,
             "target_time": target_time,
         }
-        return await self.collection.insert_one(job_data)
+        return self.collection.insert_one(job_data)
 
-    async def get_job(self, job_id: str):
-        return await self.collection.find_one({"_id": job_id})
+    def get_job(self, job_id: str):
+        return self.collection.find_one({"_id": job_id})
 
-    async def update_job(self, job_id: str, updates: dict):
-        return await self.collection.update_one({"_id": job_id}, {"$set": updates})
+    def update_job(self, job_id: str, updates: dict):
+        return self.collection.update_one({"_id": job_id}, {"$set": updates})
 
-    async def delete_job(self, job_id: str):
-        return await self.collection.delete_one({"_id": job_id})
+    def delete_job(self, job_id: str):
+        return self.collection.delete_one({"_id": job_id})
 
-    async def get_all_jobs(self):
-        cursor = self.collection.find({})
-        return await cursor.to_list(length=None)
+    def get_all_jobs(self):
+        return list(self.collection.find({}))
 
-    async def upsert_job(self, job_id: str, job_data: dict):
-        return await self.collection.update_one(
-            {"job_id": job_id}, {"$set": job_data}, upsert=True
-        )
+    def upsert_job(self, job_id: str, job_data: dict):
+        return self.collection.update_one({"job_id": job_id}, {"$set": job_data}, upsert=True)
 
 
 class RepositoryManager:
     def __init__(self, uri: str, db_name: str):
         self.client = MongoClient(uri, tlsCAFile=certifi.where())
         self.db = self.client[db_name]
+
         self.users = MongoRepository(self.db["users"])
         self.paid_subscribers = MongoRepository(self.db["paid_subscribers"])
         self.token_base = MongoRepository(self.db["token_base"])
@@ -103,5 +101,6 @@ class RepositoryManager:
         self.transactions = MongoRepository(self.db["transactions"])
         self.llmcost = MongoRepository(self.db["llm_cost"])
         self.creators = MongoRepository(self.db["creators"])
+
 
 repo_manager = RepositoryManager(uri=MONGO_STRING, db_name=DATABASE_NAME)

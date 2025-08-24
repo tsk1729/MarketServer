0632cd27c8016c3803b7458bcb95c25e41b0cc22
Sandeep <talapally.kumar@oracle.com>
2025-08-24 23:29:13 +0530

    cors init

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from creators.controller import creator_controller
 
 app = FastAPI()
 app.include_router(creator_controller)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
